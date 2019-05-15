bc37b85084cc04acc644beba3140157df2cd4e09
Deep Ray <deep.ray@gmail.com>
2019-05-15 11:05:31 +0200

    Removed Code1D and code1d

--- code1D/Common1D/Tcell_write1D.m
+++
@@ -1,9 +0,0 @@
-function Tcell_write1D(fid,t,xc)
-
-fprintf(fid, '%.6f', t);
-for k=1:length(xc)
-    fprintf(fid, ', %.6f', xc);
-end
-fprintf(fid, '\n');
-
-return
--- code1D/Scalar/Scalar1D_limit.m
+++
@@ -1,6 +0,0 @@
-function u = Scalar1D_limit(u,ind,bc_cond,Limiter,Mesh)
-
-u_ext = Apply_BC1D(u,bc_cond);
-u     = SlopeLimit1D(u_ext,ind,Limiter,Mesh);
-
-return
